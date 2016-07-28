03c92a8fa97b24eda7be4c3330c0cd55d3791685
elliotjberman <elliotjberman@gmail.com>
2016-07-28 18:13:29 -0400

    whoops

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ const uploadPhotos = (req, res) => {
             console.log("Uploading: " + filename);
 
             //Path where image will be uploaded
-            fstream = fs.createWriteStream(__dirname + 'public/photos/' + filename);
+            fstream = fs.createWriteStream(__dirname + '/public/photos/' + filename);
             file.pipe(fstream);
             fstream.on('close', function () {
                 console.log("Upload Finished of " + filename);
